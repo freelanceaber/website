bd84c00fa8123fb1e433e789185ed1eea398b013
Julien Brun <julienj.brun@gmail.com>
2023-11-29 16:28:24 +0000

    directory

--- src/_data/directory.js
+++ src/_data/directory.js
@@ -1,5 +1,4 @@
 module.exports = {
-    en: {
         freelancers: [
             {
                 "name": "Alexander Holloway",
@@ -10,15 +9,28 @@ module.exports = {
                 "link": "https://www.facebook.com/joyfullifeguide",
             },
             {
-                "name": "Alexander Holloway",
-                "gig": "Coach",
-                "business": "The Joyful Life Guide",
-                "bio": "Life coaching grounded in holistic wellbeing practices, children’s yoga classes, dance meditation facilitation",
-                "email": "joyfullifeguide@gmail.com",
-                "link": "https://www.facebook.com/joyfullifeguide",
+                "name": "Jo Hollowood",
+                "gig": "Graphic designer",
+                "business": "JHGD & Associates",
+                "bio": "Brand and identity, design and print, website design and development, exhibition design, animation",
+                "email": "jo@jhgdassociates.co.uk",
+                "link": "https://jhgdassociates.co.uk/",
             },
+            {
+                "name": "Harriet McDevitt-Smith",
+                "gig": "Artist, graphic designer and photographer",
+                "bio": "brand identity and design, graphic design, web design, photography and bespoke art projects",
+                "business": "HarriART",
+                "link": "https://www.harriartandphotography.co.uk/",
+                "email": "hmsproductions@live.co.uk"
+            },
+            {
+                    "name": "Tony Drane",
+                    "gig": "Production manager",
+                    "business": "360-TV",
+                    "bio": "audio visual, broadcast, live streaming, video, editing, event production",
+                    "link": "https://www.360-tv.net/",
+                    "email": "studio1@360-tv.net"
+            }
         ]
-    },
-    cy: {
-    }
 }
