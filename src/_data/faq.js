081f752233b1aa6419ee801bf016d40502b915bd
Julien Brun <julienj.brun@gmail.com>
2023-11-29 18:01:07 +0000

    faq

---
+++ src/_data/faq.js
@@ -0,0 +1,32 @@
+module.exports = {
+    questions: [
+        {
+            "question": "Who can join Freelance Aberystwyth Llawrydd?",
+            "answer": "Freelancers, independent contractors and remote workers are all welcome to join our community. Whether you're a seasoned freelancer or just starting out, our group is open to individuals from various industries and backgrounds."
+        },
+        {
+            "question": "Are there any membership fees?",
+            "answer": "Freelance Aberystwyth is committed to keeping our community accessible. Currently, membership is free, allowing freelancers of all levels to participate in our events and connect with fellow professionals."
+        },
+        {
+            "question": "What types of events does Freelance Aberystwyth Llawrydd organize?",
+            "answer": "We host a variety of events, including networking mixers, skill-sharing sessions, workshops, and panel discussions. Our goal is to provide opportunities for freelancers to learn, grow, and build meaningful connections within the local community."
+        },
+        {
+            "question": "How can I stay updated on upcoming events?",
+            "answer": "Stay in the loop by regularly checking our website for event announcements. Additionally, you can join our mailing list, facebook and whatsapp group."
+        },
+        {
+            "question": "Can I propose an event or workshop idea?",
+            "answer": "Absolutely! We encourage our members to contribute to the community by suggesting event ideas or volunteering to lead workshops. Reach out to our network coordinator or mention it during one of our gatherings."
+        },
+        {
+            "question": "Is Freelance Aberystwyth only for professionals in specific industries?",
+            "answer": "Not at all! Our community is diverse, welcoming freelancers from various industries such as design, construction, writing, stallholders, programming, marketing, and more. The strength of our community lies in the diversity of skills and experiences our members bring to the table."
+        },
+        {
+            "question": "How can Freelance Aberystwyth benefit my freelance career?",
+            "answer": "By joining Freelance Aberystwyth, you'll have the opportunity to expand your professional network, gain insights into different industries, and stay updated on the latest trends and best practices. Our events are designed to provide valuable resources and support to help you thrive in your freelance journey."
+        }
+    ]
+};
