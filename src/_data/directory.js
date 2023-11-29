d2261455e29e22f62704d05a1a30593811174899
Julien Brun <julienj.brun@gmail.com>
2023-11-29 19:37:39 +0000

    more people in freelancer directory

--- src/_data/directory.js
+++ src/_data/directory.js
@@ -17,6 +17,14 @@ module.exports = {
                 "link": "https://jhgdassociates.co.uk/",
             },
             {
+                "name": "Steven Swindells",
+                "gig": "Piano & Voice",
+                "business": "Abermusic",
+                "bio": "Steven Swindells is a British tenor, Royal College of Music graduate, and former Birmingham Conservatoire piano performance student. He's performed on TV and radio, with credits including Beethoven’s 9th Symphony and roles with British Youth Opera. ",
+                "link": "https://abermusic.wales/",
+                "email": "hello@abermusic.wales"
+            },
+            {
                 "name": "Harriet McDevitt-Smith",
                 "gig": "Artist, graphic designer and photographer",
                 "bio": "brand identity and design, graphic design, web design, photography and bespoke art projects",
@@ -31,6 +39,94 @@ module.exports = {
                     "bio": "audio visual, broadcast, live streaming, video, editing, event production",
                     "link": "https://www.360-tv.net/",
                     "email": "studio1@360-tv.net"
+            },
+            {
+                "name": "Caroline Juler",
+                "gig": "Film-maker, writer & editor ",
+                "business": "Mamaliga",
+                "bio": "Promotional and documentary videos for artists and community groups; articles, lectures and books on art, culture and ethnography; English-language checking of academic journals",
+                "link": "https://www.mamaliga.co.uk/",
+                "email": "carolinejuler@gmail.com"
+            },
+            {
+                "name": "Kerry Ferguson",
+                "gig": "Digital marketing & social media trainer",
+                "business": "Cambrian Web",
+                "bio": "I help small businesses like you feel confident with their digital marketing.",
+                "link": "https://kerryferguson.co.uk/",
+                "email": "kerry@kerryferguson.co.uk"
+            },
+            {
+                "name": "Lucy Kemish",
+                "gig": "Digital marketing & social media",
+                "business": "LK Digital Marketing",
+                "bio": " I specialise in social media management and training for therapists.",
+                "link": "https://www.lkdigitalmarketing.com/",
+                "email": "contact@lkdigitalmarketing.com"
+            },
+            {
+                "name": "Janina Haugg",
+                "gig": "Translator, English/German",
+                "business": "JH Translations",
+                "bio": "I offer translations from English into German in the fields of marketing, e-commerce and business, and I’m happy to consider projects in other areas.",
+                "link": "http://www.janinahaugg.de/",
+                "email": "mail@janinahaugg.de"
+            },
+            {
+                "name": "Keri Thomas",
+                "gig": "Private Tutor",
+                "business": "Aber tutoring",
+                "bio": "Tuition in GCSE and A-Level English Literature - delivered digitally ",
+                "link": "https://www.facebook.com/abertutoring",
+                "email": "abertutoring@gmail.com"
+            },
+            {
+                "name": "Emlyn Jones",
+                "gig": "Website developer",
+                "business": "Cambian Web",
+                "bio": "Bilingual Web design and GIS industry. Skilled in ArcGIS, Environmental Issues, Web Design, and Management. Strong entrepreneurship professional",
+                "link": "https://cambrianweb.com/",
+                "email": "info@cambrianweb.com"
+            },
+            {
+                "name": "Jane MacNamee",
+                "gig": "Nature writer and editor",
+                "business": "Writes",
+                "bio": "I'm a non-fiction writer and editor with over fifteen years' experience working in publishing. ",
+                "link": "https://janemacnamee.co.uk/",
+                "email": "janemacnamee@btinternet.com"
+            },
+            {
+                "name": "Jane Powell",
+                "gig": "Writer, editor and teacher",
+                "business": "Food Society",
+                "bio": "I write, edit, teach and organize events, in English and Welsh",
+                "link": "https://www.foodsociety.wales/",
+                "email": "jane@foodsociety.wales"
+            },
+            {
+                "name": "Richard Steele",
+                "gig": "Copywriter & editor",
+                "business": "",
+                "bio": "",
+                "link": "https://www.richardsteele.org/",
+                "email": "richard@richardsteele.org"
+            },
+            {
+                "name": "Dion Winton-Polak",
+                "gig": "Editor & proofreader",
+                "business": "The Fine-toothed Comb",
+                "bio": "Because words matter.Whether we're writing fact or fiction, we all have a story to tell. I know how hard it can be to evoke the right feelings, to communicate the right message, to carry your readers with you. It is my job – and my pleasure – to help elevate your work. My name is Dion Winton-Polak. ",
+                "link": "https://thefinetoothed.com/",
+                "email": "dion@thefinetoothed.com"
             }
+            // {
+            //     "name": "",
+            //     "gig": "",
+            //     "business": "",
+            //     "bio": "",
+            //     "link": "",
+            //     "email": ""
+            // }
         ]
 }
