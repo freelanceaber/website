33a33660d1e31313e3aa98a74c44e85e4e965a6c
Julien Brun <julienj.brun@gmail.com>
2024-01-04 15:47:10 +0000

    january 2024 update 1

--- src/_data/directory.js
+++ src/_data/directory.js
@@ -1,196 +1,211 @@
-module.exports = {
-    freelancers: [
-        {
-            "name": "Alexander Holloway",
-            "gig": "Coach",
-            "business": "The Joyful Life Guide",
-            "bio": "Life coaching grounded in holistic wellbeing practices, children’s yoga classes, dance meditation facilitation",
-            "email": "joyfullifeguide@gmail.com",
-            "link": "https://www.facebook.com/joyfullifeguide",
-        },
-        {
-            "name": "Jo Hollowood",
-            "gig": "Graphic designer",
-            "business": "JHGD & Associates",
-            "bio": "Brand and identity, design and print, website design and development, exhibition design, animation",
-            "email": "jo@jhgdassociates.co.uk",
-            "link": "https://jhgdassociates.co.uk/",
-        },
-        {
-            "name": "Steven Swindells",
-            "gig": "Piano & Voice",
-            "business": "Abermusic",
-            "bio": "Steven Swindells is a British tenor, Royal College of Music graduate, and former Birmingham Conservatoire piano performance student. He's performed on TV and radio, with credits including Beethoven’s 9th Symphony and roles with British Youth Opera. ",
-            "link": "https://abermusic.wales/",
-            "email": "hello@abermusic.wales"
-        },
-        {
-            "name": "Harriet McDevitt-Smith",
-            "gig": "Artist, graphic designer and photographer",
-            "bio": "brand identity and design, graphic design, web design, photography and bespoke art projects",
-            "business": "HarriART",
-            "link": "https://www.harriartandphotography.co.uk/",
-            "email": "hmsproductions@live.co.uk"
-        },
-        {
-            "name": "Tony Drane",
-            "gig": "Production manager",
-            "business": "360-TV",
-            "bio": "audio visual, broadcast, live streaming, video, editing, event production",
-            "link": "https://www.360-tv.net/",
-            "email": "studio1@360-tv.net"
-        },
-        {
-            "name": "Caroline Juler",
-            "gig": "Film-maker, writer & editor ",
-            "business": "Mamaliga",
-            "bio": "Promotional and documentary videos for artists and community groups; articles, lectures and books on art, culture and ethnography; English-language checking of academic journals",
-            "link": "https://www.mamaliga.co.uk/",
-            "email": "carolinejuler@gmail.com"
-        },
-        {
-            "name": "Kerry Ferguson",
-            "gig": "Digital marketing & social media trainer",
-            "business": "Gwe Cambrian Web",
-            "bio": "I help small businesses like you feel confident with their digital marketing.",
-            "link": "https://kerryferguson.co.uk/",
-            "email": "kerry@kerryferguson.co.uk"
-        },
-        {
-            "name": "Lucy Kemish",
-            "gig": "Digital marketing & social media",
-            "business": "LK Digital Marketing",
-            "bio": " I specialise in social media management and training for therapists.",
-            "link": "https://www.lkdigitalmarketing.com/",
-            "email": "contact@lkdigitalmarketing.com"
-        },
-        {
-            "name": "Janina Haugg",
-            "gig": "Translator, English/German",
-            "business": "JH Translations",
-            "bio": "I offer translations from English into German in the fields of marketing, e-commerce and business, and I’m happy to consider projects in other areas.",
-            "link": "http://www.janinahaugg.de/",
-            "email": "mail@janinahaugg.de"
-        },
-        {
-            "name": "Keri Thomas",
-            "gig": "Private Tutor",
-            "business": "Aber tutoring",
-            "bio": "Tuition in GCSE and A-Level English Literature - delivered digitally ",
-            "link": "https://www.facebook.com/abertutoring",
-            "email": "abertutoring@gmail.com"
-        },
-        {
-            "name": "Emlyn Jones",
-            "gig": "Website developer",
-            "business": "Gwe Cambrian Web",
-            "bio": "Bilingual Web design and GIS industry. Skilled in ArcGIS, Environmental Issues, Web Design, and Management. Strong entrepreneurship professional",
-            "link": "https://cambrianweb.com/",
-            "email": "info@cambrianweb.com"
-        },
-        {
-            "name": "Jane MacNamee",
-            "gig": "Nature writer and editor",
-            "business": "Writes",
-            "bio": "I'm a non-fiction writer and editor with over fifteen years' experience working in publishing. ",
-            "link": "https://janemacnamee.co.uk/",
-            "email": "janemacnamee@btinternet.com"
-        },
-        {
-            "name": "Jane Powell",
-            "gig": "Writer, editor and teacher",
-            "business": "Food Society",
-            "bio": "I write, edit, teach and organize events, in English and Welsh",
-            "link": "https://www.foodsociety.wales/",
-            "email": "jane@foodsociety.wales"
-        },
-        {
-            "name": "Richard Steele",
-            "gig": "Copywriter & editor",
-            "business": "",
-            "bio": "Writing and editing for charity communications - digital and print",
-            "link": "https://www.richardsteele.org/",
-            "email": "richard@richardsteele.org"
-        },
-        {
-            "name": "Dion Winton-Polak",
-            "gig": "Editor & proofreader",
-            "business": "The Fine-toothed Comb",
-            "bio": "Because words matter.Whether we're writing fact or fiction, we all have a story to tell. I know how hard it can be to evoke the right feelings, to communicate the right message, to carry your readers with you. It is my job – and my pleasure – to help elevate your work. My name is Dion Winton-Polak. ",
-            "link": "https://thefinetoothed.com/",
-            "email": "dion@thefinetoothed.com"
-        },
-        {
-            "name": "Laura Miller",
-            "gig": "Journalist, Content Writer, Editor",
-            "business": "News Mill",
-            "bio": "Laura Miller is a freelance journalist, editor, and producer. She has a wealth of consumer finance experience, having written about money matters and business for over 15 years.",
-            "link": "https://www.lauramillerjournalist.co.uk",
-            "email": "thatlaurawrites@gmail.com"
-        },
-        {
-            "name": "Polly Ernest",
-            "gig": "Content & Creative writer, Editor",
-            "business": "",
-            "bio": "Have worked writing for thirty plus years - a hired pen. Currently doing SEO content writing, editing an online NGO news round up and doing a creative writing PHD!",
-            "link": "https://pollyernest.medium.com/",
-            "email": "pollyernest@me.com"
-        },
-        {
-            "name": "Gabriella Bosticco",
-            "gig": "Copywriter",
-            "business": "",
-            "bio": "I have experience working with e-commerce businesses and other professionals in a range of industries. This includes content like blog posts, social media management, Google Business content and metadata, along with archive updating. ",
-            "link": "https://gjbosticco.wordpress.com/",
-            "email": "gj.bosticco@yahoo.com"
-        },
-        {
-            "name": "Magdalena Mazur",
-            "gig": "Vocal Coach",
-            "business": "Vocalize Studio",
-            "bio": "Online and studio vocal coaching with certified vocal coach",
-            "link": "https://www.vocalizestudio.co.uk/",
-            "email": "info@vocalizestudio.co.uk"
-        },
-        {
-            "name": "Heulwen Davies",
-            "gig": "Bilingual Marketing & PR",
-            "business": "Llais Cymru",
-            "bio": "With 13+ years experience in bilingual marketing & PR, I’m here to help businesses & organisations to grow. I can help you reach more customers, sell more & raise your profile. I offer a full marketing & PR service & I also run 1:1 live business marketing courses tailored for the clients needs. ",
-            "link": "https://www.llaiscymru.wales",
-            "email": "heulwen@llaiscymru.wales"
-        },
-        {
-            "name": "Claire Wilkins",
-            "gig": "Graphic design, motion graphics, video editing ",
-            "business": "Eye4aneye",
-            "bio": "I provide creative solutions for digital and print media. Social media content creation to video editing. ",
-            "link": "https://www.eye4aneye.co.uk/",
-            "email": "design@eye4aneye.co.uk"
-        },
-        {
-            "name": "Alison Pierse",
-            "gig": "Artist",
-            "business": "Aberdabbadoo",
-            "bio": "Aberdabbadoo is the collective name for a business that runs heritage walks around Aberystwyth, makes mosaics for kitchens, gardens and bathrooms and teaches the art of mosaic.",
-            "link": "https://aberdabbadoo.com/",
-            "email": "alisonpierse@gmail.com"
-        },
-        {
-            "name": "Lucy McQuillan",
-            "gig": "Guide",
-            "business": "Forest Bathing Practice Guide",
-            "bio": "Forest Bathing Practice Guide - A form of mindfulness, but in nature. Sessions organised for one person up to ten. Huge range of health and wellbeing benefits for participants.",
-            "link": "https://www.forestbathingandmindfulness.com",
-            "email": "lucyforestbathing@gmail.com"
-        }
-        // {
-        //     "name": "",
-        //     "gig": "",
-        //     "business": "",
-        //     "bio": "",
-        //     "link": "",
-        //     "email": ""
-        // }
-    ]
+/**
+ * @description Freelancers Directory Entries. Just add new entries at the end, entries are automatically sorted alphabetically during site build
+ */
+const freelancers = [
+    {
+        "name": "Alexander Holloway",
+        "gig": "Coach",
+        "business": "The Joyful Life Guide",
+        "bio": "Life coaching grounded in holistic wellbeing practices, children’s yoga classes, dance meditation facilitation",
+        "email": "joyfullifeguide@gmail.com",
+        "link": "https://www.facebook.com/joyfullifeguide",
+    },
+    {
+        "name": "Jo Hollowood",
+        "gig": "Graphic designer",
+        "business": "JHGD & Associates",
+        "bio": "Brand and identity, design and print, website design and development, exhibition design, animation",
+        "email": "jo@jhgdassociates.co.uk",
+        "link": "https://jhgdassociates.co.uk/",
+    },
+    {
+        "name": "Steven Swindells",
+        "gig": "Piano & Voice",
+        "business": "Abermusic",
+        "bio": "Steven Swindells is a British tenor, Royal College of Music graduate, and former Birmingham Conservatoire piano performance student. He's performed on TV and radio, with credits including Beethoven’s 9th Symphony and roles with British Youth Opera. ",
+        "link": "https://abermusic.wales/",
+        "email": "hello@abermusic.wales"
+    },
+    {
+        "name": "Harriet McDevitt-Smith",
+        "gig": "Artist, graphic designer and photographer",
+        "bio": "brand identity and design, graphic design, web design, photography and bespoke art projects",
+        "business": "HarriART",
+        "link": "https://www.harriartandphotography.co.uk/",
+        "email": "hmsproductions@live.co.uk"
+    },
+    {
+        "name": "Tony Drane",
+        "gig": "Production manager",
+        "business": "360-TV",
+        "bio": "audio visual, broadcast, live streaming, video, editing, event production",
+        "link": "https://www.360-tv.net/",
+        "email": "studio1@360-tv.net"
+    },
+    {
+        "name": "Caroline Juler",
+        "gig": "Film-maker, writer & editor ",
+        "business": "Mamaliga",
+        "bio": "Promotional and documentary videos for artists and community groups; articles, lectures and books on art, culture and ethnography; English-language checking of academic journals",
+        "link": "https://www.mamaliga.co.uk/",
+        "email": "carolinejuler@gmail.com"
+    },
+    {
+        "name": "Kerry Ferguson",
+        "gig": "Digital marketing & social media trainer",
+        "business": "Gwe Cambrian Web",
+        "bio": "I help small businesses like you feel confident with their digital marketing.",
+        "link": "https://kerryferguson.co.uk/",
+        "email": "kerry@kerryferguson.co.uk"
+    },
+    {
+        "name": "Lucy Kemish",
+        "gig": "Digital marketing & social media",
+        "business": "LK Digital Marketing",
+        "bio": " I specialise in social media management and training for therapists.",
+        "link": "https://www.lkdigitalmarketing.com/",
+        "email": "contact@lkdigitalmarketing.com"
+    },
+    {
+        "name": "Janina Haugg",
+        "gig": "Translator, English/German",
+        "business": "JH Translations",
+        "bio": "I offer translations from English into German in the fields of marketing, e-commerce and business, and I’m happy to consider projects in other areas.",
+        "link": "http://www.janinahaugg.de/",
+        "email": "mail@janinahaugg.de"
+    },
+    {
+        "name": "Keri Thomas",
+        "gig": "Private Tutor",
+        "business": "Aber tutoring",
+        "bio": "Tuition in GCSE and A-Level English Literature - delivered digitally ",
+        "link": "https://www.facebook.com/abertutoring",
+        "email": "abertutoring@gmail.com"
+    },
+    {
+        "name": "Emlyn Jones",
+        "gig": "Website developer",
+        "business": "Gwe Cambrian Web",
+        "bio": "Bilingual Web design and GIS industry. Skilled in ArcGIS, Environmental Issues, Web Design, and Management. Strong entrepreneurship professional",
+        "link": "https://cambrianweb.com/",
+        "email": "info@cambrianweb.com"
+    },
+    {
+        "name": "Jane MacNamee",
+        "gig": "Nature writer and editor",
+        "business": "Writes",
+        "bio": "I'm a non-fiction writer and editor with over fifteen years' experience working in publishing. ",
+        "link": "https://janemacnamee.co.uk/",
+        "email": "janemacnamee@btinternet.com"
+    },
+    {
+        "name": "Jane Powell",
+        "gig": "Writer, editor and teacher",
+        "business": "Food Society",
+        "bio": "I write, edit, teach and organize events, in English and Welsh",
+        "link": "https://www.foodsociety.wales/",
+        "email": "jane@foodsociety.wales"
+    },
+    {
+        "name": "Richard Steele",
+        "gig": "Copywriter & editor",
+        "business": "",
+        "bio": "Writing and editing for charity communications - digital and print",
+        "link": "https://www.richardsteele.org/",
+        "email": "richard@richardsteele.org"
+    },
+    {
+        "name": "Dion Winton-Polak",
+        "gig": "Editor & proofreader",
+        "business": "The Fine-toothed Comb",
+        "bio": "Because words matter.Whether we're writing fact or fiction, we all have a story to tell. I know how hard it can be to evoke the right feelings, to communicate the right message, to carry your readers with you. It is my job – and my pleasure – to help elevate your work. My name is Dion Winton-Polak. ",
+        "link": "https://thefinetoothed.com/",
+        "email": "dion@thefinetoothed.com"
+    },
+    {
+        "name": "Laura Miller",
+        "gig": "Journalist, Content Writer, Editor",
+        "business": "News Mill",
+        "bio": "Laura Miller is a freelance journalist, editor, and producer. She has a wealth of consumer finance experience, having written about money matters and business for over 15 years.",
+        "link": "https://www.lauramillerjournalist.co.uk",
+        "email": "thatlaurawrites@gmail.com"
+    },
+    {
+        "name": "Polly Ernest",
+        "gig": "Content & Creative writer, Editor",
+        "business": "",
+        "bio": "Have worked writing for thirty plus years - a hired pen. Currently doing SEO content writing, editing an online NGO news round up and doing a creative writing PHD!",
+        "link": "https://pollyernest.medium.com/",
+        "email": "pollyernest@me.com"
+    },
+    {
+        "name": "Gabriella Bosticco",
+        "gig": "Copywriter",
+        "business": "",
+        "bio": "I have experience working with e-commerce businesses and other professionals in a range of industries. This includes content like blog posts, social media management, Google Business content and metadata, along with archive updating. ",
+        "link": "https://gjbosticco.wordpress.com/",
+        "email": "gj.bosticco@yahoo.com"
+    },
+    {
+        "name": "Magdalena Mazur",
+        "gig": "Vocal Coach",
+        "business": "Vocalize Studio",
+        "bio": "Online and studio vocal coaching with certified vocal coach",
+        "link": "https://www.vocalizestudio.co.uk/",
+        "email": "info@vocalizestudio.co.uk"
+    },
+    {
+        "name": "Heulwen Davies",
+        "gig": "Bilingual Marketing & PR",
+        "business": "Llais Cymru",
+        "bio": "With 13+ years experience in bilingual marketing & PR, I’m here to help businesses & organisations to grow. I can help you reach more customers, sell more & raise your profile. I offer a full marketing & PR service & I also run 1:1 live business marketing courses tailored for the clients needs. ",
+        "link": "https://www.llaiscymru.wales",
+        "email": "heulwen@llaiscymru.wales"
+    },
+    {
+        "name": "Claire Wilkins",
+        "gig": "Graphic design, motion graphics, video editing ",
+        "business": "Eye4aneye",
+        "bio": "I provide creative solutions for digital and print media. Social media content creation to video editing. ",
+        "link": "https://www.eye4aneye.co.uk/",
+        "email": "design@eye4aneye.co.uk"
+    },
+    {
+        "name": "Alison Pierse",
+        "gig": "Artist",
+        "business": "Aberdabbadoo",
+        "bio": "Aberdabbadoo is the collective name for a business that runs heritage walks around Aberystwyth, makes mosaics for kitchens, gardens and bathrooms and teaches the art of mosaic.",
+        "link": "https://aberdabbadoo.com/",
+        "email": "alisonpierse@gmail.com"
+    },
+    {
+        "name": "Lucy McQuillan",
+        "gig": "Guide",
+        "business": "Forest Bathing Practice Guide",
+        "bio": "Forest Bathing Practice Guide - A form of mindfulness, but in nature. Sessions organised for one person up to ten. Huge range of health and wellbeing benefits for participants.",
+        "link": "https://www.forestbathingandmindfulness.com",
+        "email": "lucyforestbathing@gmail.com"
+    }
+    // {
+    //     "name": "",
+    //     "gig": "",
+    //     "business": "",
+    //     "bio": "",
+    //     "link": "",
+    //     "email": ""
+    // }
+];
+
+module.exports = () => {
+    return {
+        freelancers: freelancers.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        })
+    }
 }
